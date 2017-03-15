0a1664baf969de219c6cb4eb2053e5662766e42c
DalenW <dalen.ward@gmail.com>
2017-03-15 11:14:39 -0600

    started adding some data from the weather.json

--- project/Weather/java.js
+++ project/Weather/java.js
@@ -1,5 +1,7 @@
 var weatherData;
 
+myWeatherData();
+
 function weatherAPI() {
     $.ajax({
         url: "http://api.wunderground.com/api/5b0759a252c90180/geolookup/conditions/q/IA/Cedar_Rapids.json",
@@ -30,7 +32,13 @@ function setData() {
     if (weatherData == null)
         console.log("WeatherData is null");
     else {
-        document.getElementById("precipitation").innerHTML = weatherData.Franklin.Precip;
-    }
+        console.log("Setting my weather data");
 
+        document.getElementById("title").innerHTML = weatherData.Franklin.City + ": Weather Home";
+        document.getElementById("precipitation").innerHTML = weatherData.Franklin.Precip;
+        document.getElementById("forcast").innerHTML = weatherData.Franklin.Summary;
+        document.getElementById("windData").innerHTML = weatherData.Franklin.Wind + " MPH";
+        document.getElementById("location").innerHTML = weatherData.Franklin.City + ", " + weatherData.Franklin.State;
+        document.getElementById("highLow").innerHTML = "Hi / Low: " + weatherData.Franklin.High + " / " + weatherData.Franklin.Low;
+    }
 }
